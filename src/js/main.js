0dae4199e3f4ff279d2c6dd846558dbda3569362
d.kragel <d.kragel@severnaya.ru>
2024-10-05 14:12:45 +0300

    промежуточный коммит

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ $(function () {
     let promoSliderObj = $('.js__promo-slider');
 
     promoSliderObj.find('.slider').slick({
-        appendArrows: $(promoSliderObj.find('.controls'))
+        appendArrows: $(promoSliderObj.find('.controls')),
+        prevArrow: '<div class="slick-arrow slick-arrow--prev"></div>',
+        nextArrow: '<div class="slick-arrow slick-arrow--next"></div>',
     });
 });
